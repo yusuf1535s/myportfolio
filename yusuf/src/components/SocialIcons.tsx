5bad2c2c12104cfd973c726ab8f7f12dfac41468
yusuf1535s <mohammedyusuf1535s@gmail.com>
2026-03-19 15:09:04 +0530

    Updated project

--- yusuf/src/components/SocialIcons.tsx
+++ yusuf/src/components/SocialIcons.tsx
@@ -74,7 +74,7 @@ const SocialIcons = () => {
           </a>
         </span>
       </div>
-      <a className="resume-button" href="#">
+      <a className="resume-button" href="/resume.pdf" target="_blank" rel="noopener noreferrer">
         <HoverLinks text="RESUME" />
         <span>
           <TbNotes />
